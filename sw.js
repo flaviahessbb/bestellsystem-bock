74a2a477e4ab6fe32c1aa12d5dabfa91a691c616
flaviahessbb <flavia.hessbb@gmail.com>
2026-03-16 20:58:54 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'bock-v1773689498';
+const URLS = ['/'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
